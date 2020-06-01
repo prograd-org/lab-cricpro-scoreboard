c3fe2002431dd079c01f45ee2d34d564b780377c
punam-shelke <punamshelke.98@gmail.com>
2020-06-01 16:08:53 +0530

    FSJ0520A101

---
+++ home.js
@@ -0,0 +1,6 @@
+function playerDetails(id){
+    let element = document.getElementById(id);
+    element.classList.toggle("show");
+}
+//when image is clicked adding show class which makes image visible
+//when it is clicked one more time show class will be removed and by default hide class will controll visibility.
